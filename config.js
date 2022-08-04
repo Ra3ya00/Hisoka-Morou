2f4599e51b3e2d8de9b3a99d941bcadefbcfa3bf
Ra3ya00 <104101808+Ra3ya00@users.noreply.github.com>
2022-08-05 01:50:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.owner = ['966547169636']
+global.premium = ['966547169636']
+global.packname = 'luffy'
+global.author = 'luffy'
+global.sessionName = 'luffy'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
